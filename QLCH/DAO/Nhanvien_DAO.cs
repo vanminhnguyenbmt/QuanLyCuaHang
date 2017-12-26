b0de1337fdf0deed8ecfe61667170e7717550ddb
trieuit2 <trieuit2@gmail.com>
2017-12-26 16:27:46 +0700

    up

--- QLCH/DAO/Nhanvien_DAO.cs
+++ QLCH/DAO/Nhanvien_DAO.cs
@@ -14,17 +14,29 @@ namespace DAO
     {
         static SqlConnection con;
         //load nhan vien
-        public static DataTable LoadNhanVien()
+        public static List<Nhanvien_DTO> LoadNhanVien()
         {
+            string sTruyvan = "select * from NHANVIEN";
             con = DataProvider.Ketnoi();
-            var cmd = new SqlCommand("NhanVien_SelectAll", con);
-            //string sTruyvan = "select * from NHANVIEN";
-            //DataTable dt = DataProvider.LayDataTable(sTruyvan, con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            var Adapter = new SqlDataAdapter(cmd);
-            var table = new DataTable();
-            Adapter.Fill(table);
-            return table;
+            DataTable dt = DataProvider.LayDataTable(sTruyvan, con);
+            if(dt.Rows.Count==0)
+            {
+                return null;
+            }
+            List<Nhanvien_DTO> listnv = new List<Nhanvien_DTO>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Nhanvien_DTO nv = new Nhanvien_DTO();
+                nv.SMaNV = dt.Rows[i]["MANV"].ToString();
+                nv.STenNV = dt.Rows[i]["TENNV"].ToString();
+                nv.DTNgaysinh = DateTime.Parse(dt.Rows[i]["NGSINH"].ToString());
+                nv.IGioitinh = Int32.Parse(dt.Rows[i]["GIOITINH"].ToString());
+                nv.SDiachi = dt.Rows[i]["DIACHI"].ToString();
+                nv.SSDT = dt.Rows[i]["SDT"].ToString();
+                nv.SMaLoai = dt.Rows[i]["MALOAI"].ToString();
+                listnv.Add(nv);
+            }
+            return listnv;
         }
     }
 }
--- QLCH/QLCH/Program.cs
+++ QLCH/QLCH/Program.cs
@@ -16,11 +16,7 @@ namespace QLCH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new frmMain());
-=======
-            Application.Run(new frmQLNV());
->>>>>>> master
         }
     }
 }
